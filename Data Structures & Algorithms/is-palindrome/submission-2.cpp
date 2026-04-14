cce8ae61c46b662cc2885615347e3d347975ad9a
Jay <127246162+JMyoi@users.noreply.github.com>
2026-04-13 20:16:52 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,56 @@
+#include <cctype> // isalnum()
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //using two pointers method, 
+        //we could also parse the string and remove all non-alphanumeric characters
+        //left and right will coverge respectively and ignore 
+        //non-alphanumeric characters, 
+        //if theya re not the same then return false
+        int left = 0; 
+        int right = s.size() - 1;
+
+        while(left < right){
+            if(left > right)
+                return false;
+            //coverge both left and right until they are alphanum charactures
+            while(!isalnum(s.at(left))){
+                if(left >= right) break;
+                left++;
+            }
+            
+            while(!isalnum(s.at(right))){
+                if(left >= right) break;
+                right--;
+            }
+
+            if(tolower(s.at(left)) != tolower(s.at(right)))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
