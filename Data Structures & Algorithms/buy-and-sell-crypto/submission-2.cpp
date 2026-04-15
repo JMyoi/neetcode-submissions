4bdfdffb8028ed892a45f98038e29e5a6155eafe
Jay <127246162+JMyoi@users.noreply.github.com>
2026-04-15 11:40:30 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //using two pointer
+        //smallest buy which is left pointer, representing index
+        //biggest buy which is right pointer, representing index
+        /*
+            we want to pick smallest buy values such that 
+            the the biggest buy value is on right. 
+        */
+        //start at index zero
+        int lowestBuy = 0; // Left
+        int highestSell = 0; // Right
+        for(int i = 0; i < prices.size(); ++i){
+            //check for updating buy if we found a smaller one
+            if(prices.at(i) < prices.at(lowestBuy)){
+                lowestBuy = i; // now this is the smallest buy
+                if(highestSell < lowestBuy){ // right has to stay right
+                    highestSell = lowestBuy;
+                }
+            }
+            //check to update sell if we found a bigger one
+            if(prices.at(i) > prices.at(highestSell)){
+                highestSell = i;
+            }
+        }
+        return prices.at(highestSell) - prices.at(lowestBuy);
+
+    }
+};
