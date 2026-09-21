7bd11a195a472427a8c1d865ee2edb564d159499
Jay <127246162+JMyoi@users.noreply.github.com>
2026-09-21 16:42:41 -0400

    Add: python-dict-practice - submission-0

---
+++ Python For Beginners/python-dict-practice/submission-0.py
@@ -0,0 +1,16 @@
+from typing import Dict # this adds type hinting for Dict
+
+def count_characters(word: str) -> Dict[str, int]:
+    freq = {}
+    for w in word:
+        freq[w] = freq.get(w, 0) + 1
+    return freq
+
+
+
+
+# don't modify below this line
+print(count_characters("hello"))
+print(count_characters("world"))
+print(count_characters("hello world"))
+print(count_characters("this is a longer sentence"))
